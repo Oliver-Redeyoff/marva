b844e2afc3331d9e5e18d84e9501ba5e63a9d4c8
Oliver <oliver.redeyoff@gmail.com>
2022-05-28 14:12:26 +0100

    Cleaned main

--- src/main.py
+++ src/main.py
@@ -6,30 +6,36 @@ from modules.marva_slack import main as slack
 
 import cv2
 
-print('''
- ___      ___       __        _______  ___      ___  __      
-|"  \    /"  |     /""\      /"      \|"  \    /"  |/""\     
- \   \  //   |    /    \    |:        |\   \  //  //    \    
- /\\  \/.    |   /' /\  \   |_____/   ) \\  \/. .//' /\  \   
-|: \.        |  //  __'  \   //      /   \.    ////  __'  \  
-|.  \    /:  | /   /  \\  \ |:  __   \    \\   //   /  \\  \ 
-|___|\__/|___|(___/    \___)|__|  \___)    \__/(___/    \___)
-                                                             
-''')
-face_recognition.init()
-speech.init()
-slack.init()
 
-speech.say("Hi there, my name is Marva")
+# Main loop
+def main():
+    while(True):
 
-while(True):
+        # see what faces are visible
+        (faces, image) = face_recognition.get_faces()
+        
+        # send update via slack
+        slack.send("I can see " + ", ".join([face.first_name for face in faces]))
+        cv2.imwrite("test.png", image)
+        slack.send_file("./test.png")
 
-    # see what faces are visible
-    (faces, image) = face_recognition.get_faces()
-    
-    # send update via slack
-    slack.send("I can see " + ", ".join([face.first_name for face in faces]))
-    cv2.imwrite("test.png", image)
-    slack.send_file("./test.png")
+        time.sleep(5)
 
-    time.sleep(1)
+
+# Driver code
+if __name__ == "__main__":
+    print('''
+    ___      ___       __        _______  ___      ___  __      
+    |"  \    /"  |     /""\      /"      \|"  \    /"  |/""\     
+    \   \  //   |    /    \    |:        |\   \  //  //    \    
+    /\\  \/.    |   /' /\  \   |_____/   ) \\  \/. .//' /\  \   
+    |: \.        |  //  __'  \   //      /   \.    ////  __'  \  
+    |.  \    /:  | /   /  \\  \ |:  __   \    \\   //   /  \\  \ 
+    |___|\__/|___|(___/    \___)|__|  \___)    \__/(___/    \___)
+                                                                
+    ''')
+    face_recognition.init()
+    speech.init()
+    slack.init()
+
+    speech.say("Hi there, my name is Marva")
